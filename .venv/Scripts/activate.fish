baf081037995f2a8d6580da8836be26328466872
NafisaKhan <nafisakhan0511@gmail.com>
2025-07-16 17:35:18 +0600

    Recreated virtual environment and installed Streamlit

--- .venv/Scripts/activate.fish
+++ .venv/Scripts/activate.fish
@@ -33,7 +33,7 @@ end
 # Unset irrelevant variables.
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV 'e:\python\pdf chatbot\.venv'
+set -gx VIRTUAL_ENV 'E:\python\chatbot-pdf-extractor\.venv'
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/"Scripts $PATH
